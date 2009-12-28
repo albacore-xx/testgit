9bd205355cbbcfb82c8a8e68727daa273991c712
albacore <albacorefr@gmail.com>
2009-12-28 13:36:37 +0100

    main

---
+++ src/main/java/org/albacore/main/Main.java
@@ -0,0 +1,13 @@
+package org.albacore.main;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("git repository is ready");
+
+	}
+
+}
