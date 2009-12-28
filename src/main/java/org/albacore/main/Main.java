d465494b5b443adc655a235a5c35796ed3c47754
albacore <albacorefr@gmail.com>
2009-12-28 14:01:00 +0100

    commit main

--- src/main/java/org/albacore/main/Main.java
+++ src/main/java/org/albacore/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("git repository is ready");
+		System.out.println("git repository is ready  e");
 
 	}
 
